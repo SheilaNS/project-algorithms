943956965da3b04a8b283e80d317dad686f8d0b3
SheilaNS <shei.nsantos@gmail.com>
2022-11-21 15:18:44 -0300

    feat: Requisito 01 finalizado
    study_schedule implementado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    count = 0
+    for get_in, get_out in permanence_period:
+        if not isinstance(get_in, int) or not isinstance(get_out, int):
+            return None
+        if get_in <= target_time <= get_out:
+            count += 1
+    return count
