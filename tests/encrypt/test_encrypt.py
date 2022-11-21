afeb46e1bbe91fcc3db45c1c69d82792d5685593
SheilaNS <shei.nsantos@gmail.com>
2022-11-21 15:48:14 -0300

    feat: Requisito 02 finalizado
    test_encrypt_message implementado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    first = encrypt_message("AABBCC", -1)
+    assert first == "CCBBAA"
+
+    second = encrypt_message("AABBCC", 3)
+    assert second == "BAA_CCB"
+
+    third = encrypt_message("ABBCCA", 4)
+    assert third == "AC_CBBA"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("AABBCC", "-1")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 3)
